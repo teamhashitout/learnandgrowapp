8c29d198fa27d67a306e1c7735ead0cef36748d1
vivek <vivekmlohar@gmail.com>
2023-09-28 13:14:33 +0530

    Added sidebar to the APP

---
+++ app/components/Side_bar.js
@@ -0,0 +1,28 @@
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+
+
+
+const Side_bar = () => {
+    return (
+        <View style={style.Side_bar}>
+            <View style={style.profile_info}>
+                <Text style={style.text}>Heool</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    navigation: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        height: '60px',
+    },
+    icon: {
+        fontSize: "2.5rem"
+    }
+})
+export default Side_bar;
