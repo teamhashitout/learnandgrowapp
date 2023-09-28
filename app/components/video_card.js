0ad2ff123c54ef0db69ad2d1e5d74d701a2bd32f
vivek <vivekmlohar@gmail.com>
2023-09-28 15:06:38 +0530

    added asset

--- app/components/video_card.js
+++ app/components/video_card.js
@@ -1,22 +1,95 @@
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image, } from 'react-native';
 
-const video_card = () => {
-    return (
-        <View style={styles.video_card}>
-            <View style={styles.video_card__image}>
-                <Text style={styles.f}>hsf</Text>
-            </View>
-        </View>
-    )
-}
+const Video_card = () => {
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.searchBar} placeholder="Search" />
+      <View style={styles.grid}>
+        <TouchableOpacity style={styles.videoButton}>
+          <Image style={styles.videoIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.videoButton}>
+          <Image style={styles.videoIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.videoButton}>
+          <Image style={styles.videoIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.videoButton}>
+          <Image style={styles.videoIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.videoButton}>
+          <Image style={styles.videoIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.videoButton}>
+          <Image style={styles.videoIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabButton}>
+          <Image style={styles.tabIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabButton}>
+          <Image style={styles.tabIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabButton}>
+          <Image style={styles.tabIcon} source={require('../../assets/images/profile.png')} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    video_card: {
-        border: '1px solid black',
-        color: 'red',
-    },
-    icon: {
-        fontSize: "2.5rem"
-    }
-})
-export default video_card;
+  container: {
+    flex: 1,
+    backgroundColor: '#F8BBD0',
+    border: '1px solid #000000'
+  },
+  searchBar: {
+    height: 40,
+    margin: 10,
+    padding: 10,
+    borderRadius: 20,
+    backgroundColor: '#FFFFFF',
+  },
+  grid: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  videoButton: {
+    width: 150,
+    height: 150,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: '#F44336',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  videoIcon: {
+    width: 50,
+    height: 50,
+  },
+  tabBar: {
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  tabButton: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabIcon: {
+    width: 30,
+    height: 30,
+  },
+});
+
+export default Video_card;
--- app/index.js
+++ app/index.js
@@ -2,10 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { useRouter } from "expo-router";
 import Nav from "./components/Nav"
 import Footer from "./components/Footer";
-
-
-
-
+import Video_card from "./components/video_card";
 
 export default function Page() {
   return (
@@ -18,7 +15,7 @@ export default function Page() {
         <Text style={styles.user_name}>Shreya</Text>
       </View>
       <View style={styles.video_card}>
-        <video_card/>
+        <Video_card/>
       </View>
       <Footer style={styles.footer} />
      
